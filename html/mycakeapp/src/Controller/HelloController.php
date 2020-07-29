8769af0b573856f2f1c0c7646589759efc0c7964
Yuzunoha <studysteadystandby@gmail.com>
2020-07-29 11:26:14 +0900

    クエリパラメタが未指定でも問題なくなった

--- html/mycakeapp/src/Controller/HelloController.php
+++ html/mycakeapp/src/Controller/HelloController.php
@@ -8,8 +8,8 @@ class HelloController extends AppController
 {
     public function index()
     {
-        $data['name'] = $this->request->query['name'];
-        $data['pass'] = $this->request->query['pass'];
+        $data['name'] = $this->request->query['name'] ?? '';
+        $data['pass'] = $this->request->query['pass'] ?? '';
         $data['msg'] = 'こんにちは';
         $this->set('data', $data);
     }
