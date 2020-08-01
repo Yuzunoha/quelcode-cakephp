909879234eb42ef9f03faaa4e7f5476ba6ec4d53
Yuzunoha <studysteadystandby@gmail.com>
2020-08-01 23:51:44 +0900

    デフォルトレイアウトを使ってみる。超コード少ない

--- html/mycakeapp/src/Controller/HelloController.php
+++ html/mycakeapp/src/Controller/HelloController.php
@@ -8,16 +8,6 @@ class HelloController extends AppController
 {
     public function index()
     {
-        $this->viewBuilder()->autoLayout(false);
-        $this->set('title', 'Hello!');
-
-        if ($this->request->isPost()) {
-            $data = $this->request->data['Form1'];
-        } else {
-            $data = [];
-        }
-
-        $this->set('data', $data);
     }
 
     public function form()
