f69321867c951768c7b64ec3cd792ea9edf3f01b
Yuzunoha <studysteadystandby@gmail.com>
2020-08-01 20:35:37 +0900

    フォームの値受け渡し

--- html/mycakeapp/src/Controller/HelloController.php
+++ html/mycakeapp/src/Controller/HelloController.php
@@ -9,11 +9,15 @@ class HelloController extends AppController
     public function index()
     {
         $this->viewBuilder()->autoLayout(false);
-        $values = [
-            'title' => 'Hello!',
-            'message' => 'This is message!',
-        ];
-        $this->set($values);
+        $this->set('title', 'Hello!');
+
+        if ($this->request->isPost()) {
+            $data = $this->request->data['Form1'];
+        } else {
+            $data = [];
+        }
+
+        $this->set('data', $data);
     }
 
     public function form()
