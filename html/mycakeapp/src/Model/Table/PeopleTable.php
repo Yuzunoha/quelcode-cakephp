9b835385f900c46a0d01ff9366343bdb5e750726
Yuzunoha <studysteadystandby@gmail.com>
2020-08-03 00:56:11 +0900

    TableとEntityに細かい設定を書いた。デフォルト値を明示しただけなので動きは変わらない

--- html/mycakeapp/src/Model/Table/PeopleTable.php
+++ html/mycakeapp/src/Model/Table/PeopleTable.php
@@ -6,4 +6,12 @@ use Cake\ORM\Table;
 
 class PeopleTable extends Table
 {
+  public function initialize(array $config)
+  {
+    parent::initialize($config);
+
+    $this->setTable('people');
+    $this->setDisplayField('name');
+    $this->setPrimaryKey('id');
+  }
 }
