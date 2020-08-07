cf4ab0758ed4ea6062d629152df0420c125442b2
Yuzunoha <studysteadystandby@gmail.com>
2020-08-07 15:13:19 +0900

    ユーザ追加

--- html/mycakeapp/src/Controller/PeopleController.php
+++ html/mycakeapp/src/Controller/PeopleController.php
@@ -6,11 +6,25 @@ use App\Controller\AppController;
 
 class PeopleController extends AppController
 {
-
   public function index()
   {
-    $id = $this->request->query['id'] ?? 1;
-    $data = $this->People->get($id);
+    $data = $this->People->find('all');
     $this->set('data', $data);
+  }
+
+  public function add()
+  {
+    $entity = $this->People->newEntity();
+    $this->set('entity', $entity);
+  }
+
+  public function create()
+  {
+    if ($this->request->is('post')) {
+      $data =  $this->request->data['People'];
+      $entity = $this->People->newEntity($data);
+      $this->People->save($entity);
+    }
+    return $this->redirect(['action' => 'index']);
   }
 }
