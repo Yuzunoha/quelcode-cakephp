7d80c8825f8ad3c66c4555cb74de9abc95108706
Yuzunoha <studysteadystandby@gmail.com>
2020-08-15 14:31:30 +0900

    これでコインの計算やってみよう

---
+++ html/mycakeapp/src/Controller/CoinsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class CoinsController extends AppController
+{
+  public $autoRender = false;
+
+  public function index()
+  {
+    echo 'こんにちは';
+  }
+}
