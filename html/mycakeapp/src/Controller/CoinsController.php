da12b4a8f341e88452b41c7d11aaedddb9b3a786
Yuzunoha <studysteadystandby@gmail.com>
2020-08-15 15:09:46 +0900

    コイン精製

--- html/mycakeapp/src/Controller/CoinsController.php
+++ html/mycakeapp/src/Controller/CoinsController.php
@@ -10,6 +10,37 @@ class CoinsController extends AppController
 
   public function index()
   {
-    echo 'こんにちは';
+    echo '<pre>';
+    $coins = Coin::createCoinsForProbrem();
+    foreach ($coins as $idx => $coin) {
+      echo $idx . ' ... ' . $coin->getWeight() . '<br>';
+    }
+  }
+}
+
+class Coin
+{
+  protected $weight;
+  public function __construct($weight)
+  {
+    $this->weight = $weight;
+  }
+  public function getWeight()
+  {
+    return $this->weight;
+  }
+  public static function createCoinsForProbrem()
+  {
+    $num = 12;
+    $weightBasic = 50;
+    $randomOffset = (random_int(0, 1) === 0) ? -1 : 1;
+    $weightIrregular = $weightBasic + $randomOffset;
+    $irregularIdx = random_int(0, $num - 1);
+    $coins = [];
+    for ($i = 0; $i < $num; $i++) {
+      $weight = ($i === $irregularIdx) ? $weightIrregular : $weightBasic;
+      $coins[] = new Coin($weight);
+    }
+    return $coins;
   }
 }
