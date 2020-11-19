fc1b31b7eecd2c5ab3338f1de22062639bcf9615
Yuzunoha <studysteadystandby@gmail.com>
2020-11-19 22:28:18 +0900

    \App\Application::addPlugin('Migrations');

---
+++ html/mycakeapp/src/View/Helper/ProgressHelper.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\View\Helper;
+
+use Cake\View\Helper;
+use Cake\View\View;
+
+/**
+ * Progress helper
+ */
+class ProgressHelper extends Helper
+{
+    /**
+     * Default configuration.
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [];
+
+}
