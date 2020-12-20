da94acacde1ce8d1115ca5b9fb806d5a6c80c2c8
Yuzunoha <studysteadystandby@gmail.com>
2020-12-20 21:26:16 +0900

    マイグレーション実行した

--- html/mycakeapp/config/Migrations/20201220121857_CreateTopics.php
+++ html/mycakeapp/config/Migrations/20201220121857_CreateTopics.php
@@ -1,21 +1,15 @@
 <?php
+
 use Migrations\AbstractMigration;
 
 class CreateTopics extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * More information on this method is available here:
-     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
-     * @return void
-     */
     public function change()
     {
         $table = $this->table('topics');
-        $table->addColumn('user_id', 'string', [
+        $table->addColumn('user_id', 'integer', [
             'default' => null,
-            'limit' => 255,
+            'limit' => 11,
             'null' => false,
         ]);
         $table->addColumn('name', 'string', [
